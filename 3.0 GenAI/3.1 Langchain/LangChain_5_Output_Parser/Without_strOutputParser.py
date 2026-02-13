7ba31c61e5bc84baea431ad380cd6d8242419aa6
adhishpawar <adhishpawar@gmail.com>
2026-02-13 15:57:13 +0530

    With_strOutputParser_1

--- 3.0 GenAI/3.1 Langchain/LangChain_5_Output_Parser/Without_strOutputParser.py
+++ 3.0 GenAI/3.1 Langchain/LangChain_5_Output_Parser/Without_strOutputParser.py
@@ -1,21 +1,10 @@
-from huggingface_hub import InferenceClient
-from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-import os
 
 load_dotenv()
 
-hf_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-
-print(hf_token)
-
-llm = HuggingFaceEndpoint(
-    repo_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    task = "text-generation"
-)
-
-model = ChatHuggingFace(llm = llm)
+model = ChatOpenAI()
 
 # 1st report --> details Prompt
 template1 = PromptTemplate(
