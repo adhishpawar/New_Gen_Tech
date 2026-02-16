c71d55af87465fcf1babdbe5eb67e6c9995cf94c
adhishpawar <adhishpawar@gmail.com>
2026-02-16 18:17:11 +0530

    Pydantic Output parser Next is Langchain

---
+++ 3.0 GenAI/3.1 Langchain/LangChain_5_Output_Parser/WithoutChain_PydanticOutputparser.py
@@ -0,0 +1,29 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+class Person(BaseModel):
+    name: str = Field(description="Name of the person")
+    age: int = Field(gt=18, description="Age of the person")
+    city: str = Field(description="Name of the city the person belongs to")
+
+parser = PydanticOutputParser(pydantic_object=Person)
+
+template = PromptTemplate(
+    template = 'Generate the name, age and City of a Foctional {place} person \n' \
+    '{format_instruction}',
+    input_variables=['place'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+prompt = template.invoke({'place': 'indian'})
+result = model.invoke(prompt)
+final_result  = parser.parse(result.content)
+
+print(final_result)
